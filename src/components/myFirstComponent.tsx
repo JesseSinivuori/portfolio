4041358af60bff1c324aab95a89c8aae9aad6ffa
JesseSinivuori <sinvuorii@gmail.com>
2022-11-08 13:46:30 +0200

    added first component

---
+++ src/components/myFirstComponent.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type MyFirstComponentProps = {
+
+}
+
+function MyFirstComponent(props: MyFirstComponentProps) {
+
+    return (
+        <div>
+            <h1>Hello, React!</h1>
+            <p>This is my first component 😎</p>
+        </div>
+    )
+
+}
+
+export default MyFirstComponent;
