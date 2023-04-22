7b73384f9a13cf6d254b55ef6d1e530337d68713
JesseSinivuori <sinvuorii@gmail.com>
2023-04-22 21:58:41 +0300

    CSP

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const ContentSecurityPolicy = `
   style-src-elem 'self' 'unsafe-inline' fonts.googleapis.com;
   frame-src 'self' js.stripe.com;
   script-src-elem 'self' va.vercel-scripts.com js.stripe.com;
-  img-src 'self' *portfolio-one-gamma-55.vercel.app;
+  img-src 'self';
   connect-src 'self' vitals.vercel-insights.com;
   object-src 'none';
   base-uri 'none';
