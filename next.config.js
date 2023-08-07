3ce57b4f3864263e24bef80042c5c92c6327cbaf
JesseSinivuori <sinvuorii@gmail.com>
2023-08-07 13:28:06 +0300

    update CSP

--- next.config.js
+++ next.config.js
@@ -1,30 +1,25 @@
 /** @type {import('next').NextConfig} */
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval';
+  script-src 'self' ${
+    process.env.NODE_ENV === "development" ? `'unsafe-eval'` : ""
+  };
   style-src 'self' 'unsafe-inline';
   font-src 'self' fonts.gstatic.com;
   media-src 'self';
   style-src-elem 'self' 'unsafe-inline' fonts.googleapis.com;
-  frame-src 'self' js.stripe.com;
+  frame-src 'self';
   script-src-elem 'self' va.vercel-scripts.com 'unsafe-inline';
   img-src 'self' data: https://portfolio-jessesinivuori.vercel.app/;
   connect-src 'self' vitals.vercel-insights.com;
   object-src 'none';
   base-uri 'none';
 `;
+
 const securityHeaders = [
-  {
-    key: "X-DNS-Prefetch-Control",
-    value: "on",
-  },
   {
     key: "Strict-Transport-Security",
     value: "max-age=63072000; includeSubDomains; preload",
-  },
-  {
-    key: "X-XSS-Protection",
-    value: "1; mode=block",
   },
   {
     key: "Content-Security-Policy",
